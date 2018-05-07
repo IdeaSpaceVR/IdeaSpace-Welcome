9b290ccb35708f1b591800eea8c7b3ba17391f1a
Wedding DJ <chris@ideaspacevr.org>
2018-05-07 16:25:47 -0400

    impl animation for 3 constellations

--- views/partials/5_items_layout_partial.blade.php
+++ views/partials/5_items_layout_partial.blade.php
@@ -5,9 +5,39 @@
 		<!-- center //-->
 		<a-entity
 				position="0 0 0"
-				rotation="0 0 0"
-				geometry="primitive: plane; width: 1; height: 0.5"
-				material="shader: flat; src: url({{ $content['space-links'][$start_counter]['space-link-image']['space-link-image-resized']['#uri']['#value'] }})">
+				rotation="0 0 0">
+				<a-rounded 
+						position="0 0 0.0001" 
+						width="1.05" 
+						height="0.55" 
+						color="{{ $content['space-links'][$start_counter]['space-link-background-color']['#value'] }}" 
+						top-left-radius="0.04" 
+						top-right-radius="0.04" 
+						bottom-left-radius="0.04" 
+						bottom-right-radius="0.04"
+						animation__enter="property: position; dur: 500; to: 0 0 0.1; easing: easeOutElastic; startEvents:isvr_mouseenter"
+						animation__leave="property: position; dur: 200; from: 0 0 0.1; to: 0 0 0.0001; startEvents:isvr_mouseleave">
+						@if (isset($content['space-links'][$start_counter]['space-link-image']))
+						<a-entity
+								position="0 0 0.0001"
+								geometry="primitive: plane; width: 1; height: 0.5"
+								material="shader: flat; src: url({{ $content['space-links'][$start_counter]['space-link-image']['space-link-image-resized']['#uri']['#value'] }})">
+						</a-entity>
+						@endif
+						<a-entity
+								geometry="primitive: plane; width: 1; height: 0.5"
+								position="0 0 0.0002"
+								material="shader: html; target: #space-link-title-texture-cid-{{ $content['space-links'][$start_counter]['space-link-title']['#content-id'] }}; transparent: true; ratio: width">
+						</a-entity>
+						<a-entity
+								class="collidable"
+								material="opacity: 0"
+								geometry="primitive: plane; width: 1; height: 0.5"
+								position="0 0 0.0003"
+								width="1.05" 
+								height="0.55"> 
+						</a-entity>
+    		</a-rounded>
 		</a-entity>
 
 		@elseif ($count == ($start_counter + 2))
@@ -15,16 +45,76 @@
 		<!-- center //-->
 		<a-entity
 				position="0 0 0"
-				rotation="0 0 0"
-				geometry="primitive: plane; width: 1; height: 0.5"
-				material="shader: flat; src: url({{ $content['space-links'][$start_counter]['space-link-image']['space-link-image-resized']['#uri']['#value'] }})">
+				rotation="0 0 0">
+				<a-rounded 
+						position="0 0 0.0001" 
+						width="1.05" 
+						height="0.55" 
+						color="{{ $content['space-links'][$start_counter]['space-link-background-color']['#value'] }}" 
+						top-left-radius="0.04" 
+						top-right-radius="0.04" 
+						bottom-left-radius="0.04" 
+						bottom-right-radius="0.04"
+						animation__enter="property: position; dur: 500; to: 0 0 0.1; easing: easeOutElastic; startEvents:isvr_mouseenter"
+						animation__leave="property: position; dur: 200; from: 0 0 0.1; to: 0 0 0.0001; startEvents:isvr_mouseleave">
+						@if (isset($content['space-links'][$start_counter]['space-link-image']))
+						<a-entity
+								position="0 0 0.0001"
+								geometry="primitive: plane; width: 1; height: 0.5"
+								material="shader: flat; src: url({{ $content['space-links'][$start_counter]['space-link-image']['space-link-image-resized']['#uri']['#value'] }})">
+						</a-entity>
+						@endif
+						<a-entity
+								geometry="primitive: plane; width: 1; height: 0.5"
+								position="0 0 0.0002"
+								material="shader: html; target: #space-link-title-texture-cid-{{ $content['space-links'][$start_counter]['space-link-title']['#content-id'] }}; transparent: true; ratio: width">
+						</a-entity>
+						<a-entity
+								class="collidable"
+								material="opacity: 0"
+								geometry="primitive: plane; width: 1; height: 0.5"
+								position="0 0 0.0003"
+								width="1.05" 
+								height="0.55"> 
+						</a-entity>
+    		</a-rounded>
 		</a-entity>
 		<!-- center, left //-->
 		<a-entity
 				position="-1.15 0 0.09"
-				rotation="0 10 0"
-				geometry="primitive: plane; width: 1; height: 0.5"
-				material="shader: flat; src: url({{ $content['space-links'][($start_counter + 1)]['space-link-image']['space-link-image-resized']['#uri']['#value'] }})">
+				rotation="0 10 0">
+				<a-rounded 
+						position="0 0 0.0001" 
+						width="1.05" 
+						height="0.55" 
+						color="{{ $content['space-links'][$start_counter + 1]['space-link-background-color']['#value'] }}" 
+						top-left-radius="0.04" 
+						top-right-radius="0.04" 
+						bottom-left-radius="0.04" 
+						bottom-right-radius="0.04"
+						animation__enter="property: position; dur: 500; to: 0 0 0.1; easing: easeOutElastic; startEvents:isvr_mouseenter"
+						animation__leave="property: position; dur: 200; from: 0 0 0.1; to: 0 0 0.0001; startEvents:isvr_mouseleave">
+						@if (isset($content['space-links'][$start_counter + 1]['space-link-image']))
+						<a-entity
+								position="0 0 0.0001"
+								geometry="primitive: plane; width: 1; height: 0.5"
+								material="shader: flat; src: url({{ $content['space-links'][$start_counter + 1]['space-link-image']['space-link-image-resized']['#uri']['#value'] }})">
+						</a-entity>
+						@endif
+						<a-entity
+								geometry="primitive: plane; width: 1; height: 0.5"
+								position="0 0 0.0002"
+								material="shader: html; target: #space-link-title-texture-cid-{{ $content['space-links'][$start_counter + 1]['space-link-title']['#content-id'] }}; transparent: true; ratio: width">
+						</a-entity>
+						<a-entity
+								class="collidable"
+								material="opacity: 0"
+								geometry="primitive: plane; width: 1; height: 0.5"
+								position="0 0 0.0003"
+								width="1.05" 
+								height="0.55"> 
+						</a-entity>
+    		</a-rounded>
 		</a-entity>
 
 		@elseif ($count == ($start_counter + 3))
